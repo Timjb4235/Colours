e9f156b7372b9f2dd112f5a73c73fa7140abd6aa
Timjb4235 <138724261+Timjb4235@users.noreply.github.com>
2024-06-18 14:20:44 +0100

    Minor fixes

--- Colour.js
+++ Colour.js
@@ -16,11 +16,11 @@ class Colour {
     }
 
     lerp(secondColour, float) {
-        // A float of zero should return the starting colour (self), while a float of 1 should return the second colour (hex_string).
+        // A float of zero should return the starting colour (this), while a float of 1 should return the second colour.
         // Declaring the change vector from the starting colour to the second colour:
-        const colourVector = [secondColour.red - self.red, secondColourgreen - self.green, second_colourblue - self.blue];
+        const colourVector = [secondColour.red - this.red, secondColour.green - this.green, secondColour.blue - this.blue];
         // Calculating the rbg vector of the final colour:
         const finalColour = [self.red + float * colourVector[0], self.green + float * colourVector[1], self.blue + float * colourVector[2]];
-        return Colour(finalColour[0], finalColour[1], finalColour[2]);
+        return new Colour(finalColour[0], finalColour[1], finalColour[2]);
     }
 }
