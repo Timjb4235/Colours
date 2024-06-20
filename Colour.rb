963c9802cab0543722c395983179ad747e7b4273
Timjb4235 <138724261+Timjb4235@users.noreply.github.com>
2024-06-20 12:04:37 +0100

    Update to to_hex_string

--- Colour.rb
+++ Colour.rb
@@ -1,4 +1,5 @@
 class Colour
+    # Define getters and setters:
     attr_accessor :red, :green, :blue
     
     def initialize(red, green, blue)
@@ -8,9 +9,9 @@ class Colour
     end
     
     def to_hex_string
-      hex_red = @red.to_s(16).upcase
-      hex_green = @green.to_s(16).upcase
-      hex_blue = @blue.to_s(16).upcase
+      hex_red = @red.round.to_s(16).upcase
+      hex_green = @green.round.to_s(16).upcase
+      hex_blue = @blue.round.to_s(16).upcase
       hex_red.rjust(2, '0') + hex_green.rjust(2, '0') + hex_blue.rjust(2, '0')
     end
     
@@ -18,10 +19,10 @@ class Colour
         # A float of zero should return the starting colour (self), while a float of 1 should return the second colour (hex_string).
         # Declaring the change vector from the starting colour to the second colour:
         colour_vector = [second_colour.red - @red, second_colour.green - @green, second_colour.blue - @blue]
+        # Calculate final colour:
         final_red = @red + float * colour_vector[0]
         final_green = @green + float * colour_vector[1]
         final_blue = @green + float * colour_vector[2]
-        # Rounding needed
         Colour.new(final_red, final_green, final_blue)
     end
   end
